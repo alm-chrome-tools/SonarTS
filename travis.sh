d0d5c0350e886ceb01d30ad6b9f5ea9810009fe1
David Rautureau <david.rautureau@sonarsource.com>
2017-07-24 14:49:27 +0200

    Fix the path of sonar-scanner

--- travis.sh
+++ travis.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 # if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-	SCANNER_VERSION=cli-3.0.3.778-linux
+	SCANNER_VERSION=3.0.3.778-linux
 
 	# Download SonarScanner
 	mkdir -p sonar-scanner/download
-	wget "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-${SCANNER_VERSION}.zip" -O "sonar-scanner/download/sonar-scanner-${SCANNER_VERSION}.zip"
-	unzip "sonar-scanner/download/sonar-scanner-${SCANNER_VERSION}.zip" -d sonar-scanner
+	wget "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SCANNER_VERSION}.zip" -O "sonar-scanner/download/sonar-scanner-cli-${SCANNER_VERSION}.zip"
+	unzip "sonar-scanner/download/sonar-scanner-cli-${SCANNER_VERSION}.zip" -d sonar-scanner
 
 	# complete the properties file with credentials
 	echo sonar.host.url=$SONAR_HOST_URL >> sonar-project.properties
